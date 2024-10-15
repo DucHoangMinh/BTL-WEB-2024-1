9dd4f8d3015c19ad55f6bfc17711cae9f7221535
Bich Diep Tran <97890758+tranbichdiep@users.noreply.github.com>
2024-10-15 16:32:15 +0700

    update header & footer --> components

--- frontend/components/HeaderBar/index.vue
+++ frontend/components/HeaderBar/index.vue
@@ -1,9 +1,52 @@
 <template lang="pug">
-p This is header
+div
+  header
+    .header-container
+      img(src="/img/main_logo.png", alt="Logo" class="logo")
+      nav.navbar
+        ul.nav-menu
+          li.nav-item
+            a(href="/lich-chieu") Lịch Chiếu
+          li.nav-item
+            a(href="/phim") Phim
+          li.nav-item
+            a(href="/rap") Rạp
+          li.nav-item
+            a(href="/gia-ve") Giá Vé
+          li.nav-item
+            a(href="/uu-dai") Tin Mới và Ưu Đãi
+          li.nav-item
+            a(href="/nhuong-quyen") Nhượng Quyền
+          li.nav-item
+            a(href="/thanh-vien") Thành Viên
 </template>
 <script setup>
 
 </script>
 <style scoped lang="sass">
+/* Header styles */
+.header-container
+  display: flex
+  justify-content: space-between
+  align-items: center
+  padding: 20px
+  background-color: white
+  border-bottom: 1px solid #eaeaea
 
+.logo
+  width: 150px
+
+.navbar
+  ul.nav-menu
+    list-style: none
+    display: flex
+    gap: 20px
+
+    li.nav-item
+      a
+        text-decoration: none
+        color: black
+        font-weight: bold
+        &:hover
+          color: #007bff
 </style>
