79670bb33a44d8a442194f258dda662412619af0
Dong1311 <thanhdong1311@gmail.com>
2024-10-18 15:42:05 +0700

    add movie controller

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const registerRouter = require('./register');
 const loginRouter = require('./login');
+const movieRouter = require('./movies');
 
 const authenticateToken = require('../middleware/prismaAuthMiddleware');  
 
 function route(app) {
   app.use('/register', registerRouter);
   app.use('/login', loginRouter);
-
+  app.use('/movies', movieRouter);  
+  
   app.get('/protected', authenticateToken, (req, res) => {
     res.json({
       message: 'This is a protected route. You are authenticated.',
