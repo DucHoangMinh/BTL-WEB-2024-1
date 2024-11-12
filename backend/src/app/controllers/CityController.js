974aa49f5a3d46cb21bcaf6c0239136ba9b87a36
unknown <trinhdat1333@gmail.com>
2024-11-13 01:11:01 +0700

    ok

---
+++ backend/src/app/controllers/CityController.js
@@ -0,0 +1,44 @@
+// controllers/CityController.js
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+class CityController {
+ 
+  async getTheatersAndMoviesByCity(req, res) {
+    const { cityName } = req.params; 
+    console.log(cityName)
+    try {
+    
+      const theaters = await prisma.movieTheater.findMany({
+        where: { city: cityName },
+        // include: {
+        //   Showtimes: {
+        //     include: {
+        //       Movie: true
+        //     }
+        //   }
+        // }
+      });
+
+      // const movies = [];
+      // const movieSet = new Set();
+
+      // theaters.forEach(theater => {
+      //   theater.Showtimes.forEach(showtime => {
+      //     if (!movieSet.has(showtime.Movie.id)) {
+      //       movieSet.add(showtime.Movie.id);
+      //       movies.push(showtime.Movie);
+      //     }
+      //   });
+      // });
+
+      
+      res.status(200).json({ theaters });
+    } catch (error) {
+      console.error('Error fetching theaters and movies by city:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = new CityController();
