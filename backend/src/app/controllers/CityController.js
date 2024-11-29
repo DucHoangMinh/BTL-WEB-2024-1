b89dcbbcfb37377a8632747556995aeed0f2bbb3
unknown <trinhdat1333@gmail.com>
2024-11-30 01:30:01 +0700

    luong 2

--- backend/src/app/controllers/CityController.js
+++ backend/src/app/controllers/CityController.js
@@ -6,11 +6,10 @@ class CityController {
  
   async getTheatersAndMoviesByCity(req, res) {
     const { cityName } = req.params; 
-    console.log(cityName)
     try {
     
       const theaters = await prisma.movieTheater.findMany({
-        where: { city: cityName },
+        where: { city: cityName, select: { name: true } },
         // include: {
         //   Showtimes: {
         //     include: {
@@ -33,12 +32,74 @@ class CityController {
       // });
 
       
-      res.status(200).json({ theaters });
+      const theaterNames = theaters.map(theater => theater.name);
+
+      return res.status(200).json({ theaterNames });
     } catch (error) {
       console.error('Error fetching theaters and movies by city:', error);
       res.status(500).json({ message: 'Internal Server Error' });
     }
   }
+
+  async getRoomFromTheater(req,res) {
+      const {theaterName} = req.params;
+      try {
+        const roomIds = await prisma.room.findMany({
+          where:{ theaters: theaterName, select: {id: true}}
+        });
+        const roomids = roomIds.map(room => room.id);
+        return res.status(200).json(roomids);
+      } catch (error) {
+        console.error('Error fetching rooms:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+      }
+  }
+
+  async getShowtimesByMovie(req, res) {
+    const { movieId } = req.params;
+    const { date } = req.query;
+
+    if ( !movieId || !date) {
+      return res.status(400).json({ message: 'theaterId, movieId, and date are required' });
+    }
+
+    try {
+      const showtimes = await prisma.showtime.findMany({
+        where: {
+          movie_id: parseInt(movieId),
+          show_date: new Date(date), // Thêm điều kiện ngày để lọc theo ngày được chọn
+          
+        },
+        select: {
+          id: true,
+          show_date: true,
+          start_time: true,
+          end_time: true,
+          price: true,
+          Room: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+
+      if (showtimes.length === 0) {
+        return res.status(404).json({ message: 'No showtimes found for this movie at the selected theater on the chosen date' });
+      }
+
+      return res.status(200).json({
+        message: 'Danh sách suất chiếu',
+        showtimes,
+      });
+    } catch (error) {
+      console.error('Error fetching showtimes:', error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+  
+
+  async 
 
 
 }
