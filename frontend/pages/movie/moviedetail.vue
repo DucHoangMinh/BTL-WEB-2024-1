fc501a768eeb70e22a20eaba1beb7ea179e5ab3a
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-12-02 22:35:30 +0700

    add some codee

--- frontend/pages/movie/moviedetail.vue
+++ frontend/pages/movie/moviedetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="movie-detail">
-    <!-- Poster và thông tin phim -->
+    <!-- Poster vĂ  thĂ´ng tin phim -->
     <div class="movie-header">
       <img :src="movie.thumbnail" alt="Movie Poster" class="movie-poster" />
       <div class="movie-info">
@@ -15,9 +15,8 @@
       </div>
     </div>
 
-    <!-- Mô tả phim -->
     <div class="movie-description mb-6">
-      <h2>Tóm tắt</h2>
+      <h2>TĂ³m táº¯t</h2>
       <p>{{ movie.description }}</p>
     </div>
 
@@ -27,13 +26,12 @@
       </video>
     </div>
 
-    <!-- Thêm component BookingPopup -->
-    <BookingPopup 
-      :is-open="isBookingPopupOpen"
-      :movie-id="movieId"
-      @close="closeBookingPopup"
-      v-if="isBookingPopupOpen"
-      @finish_choose_place="finishChoosePlace"
+    <BookingPopup
+        :is-open="isBookingPopupOpen"
+        :movie-id="movieId"
+        @close="closeBookingPopup"
+        v-if="isBookingPopupOpen"
+        @finish_choose_place="finishChoosePlace"
     />
   </div>
 </template>
@@ -41,6 +39,8 @@
 <script setup>
 import axios from "axios";
 import {previousPageStore} from "~/stores/previousPage.js";
+import showMessages from "~/utils/toast.js";
+
 const previousPageStoreRef = previousPageStore()
 
 const route = useRoute()
@@ -50,29 +50,34 @@ const isBookingPopupOpen = ref(false)
 const movieId = ref(null)
 
 const finishChoosePlace = (data) => {
-  console.log(data.selectedDate + " " + data.selectedShowTimeId + " " + data.selectedTheaterId)
-  // Validate data before go to sheet page
-
-  router.push(`/seat-select?room=1&showtime=${data.selectedShowTimeId}&movie_id=${route.query['id']}&theater_id=${data.selectedTheaterId}`)
+  if (data.selectedTheaterId == null) {
+    showMessages.warning("Vui lòng chọn địa điểm và cụm rạp bạn muốn xem phim!")
+    return
+  }
+  if (data.selectedShowTimeId == null) {
+    showMessages.warning("Vui lòng chọn khung giờ bạn muốn xem phim!")
+    return
+  }
+  router.push(`/seat-select?room=${data.selectedRoomId}&showtime=${data.selectedShowTimeId}&movie_id=${route.query['id']}&theater_id=${data.selectedTheaterId}`)
 }
 
 const getMovieDetail = async () => {
-  try{
+  try {
     const movieId = route.query.id
-    const { data } = await axios.get(`https://api-btl-web-2024-1.vercel.app/movies/${movieId}`)
+    const {data} = await axios.get(`https://api-btl-web-2024-1.vercel.app/movies/${movieId}`)
     movie.value = data.movie
     console.log(movie.value)
-  } catch (e){
+  } catch (e) {
     console.log(e)
-    alert("Có lỗi xảy ra, vui lòng liên hệ với nhà quản trị trang web!")
+    alert("CĂ³ lá»—i xáº£y ra, vui lĂ²ng liĂªn há»‡ vá»›i nhĂ  quáº£n trá»‹ trang web!")
     await router.push('/')
   }
 }
-const formatDateToDDMMYYYY = (dateString) =>  {
+const formatDateToDDMMYYYY = (dateString) => {
   const date = new Date(dateString);
 
   const day = String(date.getUTCDate()).padStart(2, '0');
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
   const year = date.getUTCFullYear();
 
   return `${day}/${month}/${year}`;
@@ -134,7 +139,8 @@ const closeBookingPopup = () => {
   border-radius: 5px;
   margin-top: 20px;
 }
-.trailer-button{
+
+.trailer-button {
   background-color: #6c757d;
   color: white;
   padding: 10px 20px;
@@ -144,7 +150,8 @@ const closeBookingPopup = () => {
   border-radius: 5px;
   margin-top: 20px;
 }
-.trailer-button:hover{
+
+.trailer-button:hover {
   background-color: #495057;
 }
 
